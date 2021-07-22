51f0674deece0831148d9342128afc7ff8d5dd93
Jeremy Camacho Lugo <jyaclu@gmail.com>
2021-07-22 06:42:06 +0100

    added advanced filters skeleton for beaches

--- components/AdvancedSearchFilter.vue
+++ components/AdvancedSearchFilter.vue
@@ -233,6 +233,324 @@
                     </v-list>
                 </v-col>
             </v-row>
+            <v-row>
+                <v-col class="py-0 pt-1" cols="8">
+                    <h4>Playa nudista</h4>
+                </v-col>
+                <v-spacer></v-spacer>
+                <v-col class="pa-0" cols="3">
+                    <v-switch class="ma-0"
+                        v-model="nudismFilter"
+                    ></v-switch>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col class="py-0 pt-1" cols="8">
+                    <h4>Bandera azul</h4>
+                </v-col>
+                <v-spacer></v-spacer>
+                <v-col class="pa-0" cols="3">
+                    <v-switch class="ma-0"
+                        v-model="blueFlagFiler"
+                    ></v-switch>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col class="py-0 pt-1" cols="8">
+                    <h4>Socorrista</h4>
+                </v-col>
+                <v-spacer></v-spacer>
+                <v-col class="pa-0" cols="3">
+                    <v-switch class="ma-0"
+                        v-model="lifeGuardFilter"
+                    ></v-switch>
+                </v-col>
+            </v-row>
+            <v-row align-content="center">
+                <v-col class="py-0 pt-1" cols="8">
+                    <h4>Acceso para discapacitados</h4>
+                </v-col>
+                <v-spacer></v-spacer>
+                <v-col class="pa-0" cols="3">
+                    <v-switch class="ma-0"
+                        v-model="disabledAccessFilter"
+                    ></v-switch>
+                </v-col>
+            </v-row>
+            <v-row align-content="center">
+                <v-col class="py-0 pt-1" cols="8">
+                    <h4>Parking</h4>
+                </v-col>
+                <v-spacer></v-spacer>
+                <v-col class="pa-0" cols="3">
+                    <v-switch class="ma-0"
+                        v-model="parkingFilter"
+                    ></v-switch>
+                </v-col>
+            </v-row>
+            <v-row align-content="center">
+                <v-col class="py-0 pt-1" cols="8">
+                    <h4>Duchas</h4>
+                </v-col>
+                <v-spacer></v-spacer>
+                <v-col class="pa-0" cols="3">
+                    <v-switch class="ma-0"
+                        v-model="showerFilter"
+                    ></v-switch>
+                </v-col>
+            </v-row>
+            <v-row align-content="center">
+                <v-col class="py-0 pt-1" cols="8">
+                    <h4>Alquiler de sombrillas</h4>
+                </v-col>
+                <v-spacer></v-spacer>
+                <v-col class="pa-0" cols="3">
+                    <v-switch class="ma-0"
+                        v-model="rentalSunUmbrellaFilter"
+                    ></v-switch>
+                </v-col>
+            </v-row>
+            <v-row align-content="center">
+                <v-col class="py-0 pt-1" cols="8">
+                    <h4>Alquiler de tumbonas</h4>
+                </v-col>
+                <v-spacer></v-spacer>
+                <v-col class="pa-0" cols="3">
+                    <v-switch class="ma-0"
+                        v-model="rentalHamocksFilter"
+                        dense
+                    ></v-switch>
+                </v-col>
+            </v-row>
+            <v-row align-content="center">
+                <v-col class="py-0 pt-1" cols="8">
+                    <h4>Alquiler de barcos</h4>
+                </v-col>
+                <v-spacer></v-spacer>
+                <v-col class="pa-0" cols="3">
+                    <v-switch class="ma-0"
+                        v-model="rentalBoatsFilter"
+                        dense
+                    ></v-switch>
+                </v-col>
+            </v-row>
+            <v-row align-content="center">
+                <v-col class="py-0 pt-1" cols="8">
+                    <h4>Comida</h4>
+                </v-col>
+                <v-spacer></v-spacer>
+                <v-col class="pa-0" cols="3">
+                    <v-switch class="ma-0"
+                        v-model="foodFilter"
+                        dense
+                    ></v-switch>
+                </v-col>
+            </v-row>
+            <v-row align-content="center">
+                <v-col class="py-0 pt-1" cols="8">
+                    <h4>Bebida</h4>
+                </v-col>
+                <v-spacer></v-spacer>
+                <v-col class="pa-0" cols="3">
+                    <v-switch class="ma-0"
+                        v-model="drinksFilter"
+                        dense
+                    ></v-switch>
+                </v-col>
+            </v-row>
+            <v-row align-content="center">
+                <v-col class="py-0 pt-1" cols="8">
+                    <h4>Zona para niños</h4>
+                </v-col>
+                <v-spacer></v-spacer>
+                <v-col class="pa-0" cols="3">
+                    <v-switch class="ma-0"
+                        v-model="childZoneFilter"
+                        dense
+                    ></v-switch>
+                </v-col>
+            </v-row>
+            <v-row align-content="center">
+                <v-col class="py-0 pt-1" cols="8">
+                    <h4>Zona deportiva</h4>
+                </v-col>
+                <v-spacer></v-spacer>
+                <v-col class="pa-0" cols="3">
+                    <v-switch class="ma-0"
+                        v-model="sportZoneFilter"
+                        dense
+                    ></v-switch>
+                </v-col>
+            </v-row>
+            <v-row align-content="center">
+                <v-col class="py-0 pt-1" cols="8">
+                    <h4>Submarinismo</h4>
+                </v-col>
+                <v-spacer></v-spacer>
+                <v-col class="pa-0" cols="3">
+                    <v-switch class="ma-0"
+                        v-model="scubaDivingFilter"
+                        dense
+                    ></v-switch>
+                </v-col>
+            </v-row>
+            <v-row align-content="center">
+                <v-col class="py-0 pt-1" cols="8">
+                    <h4>Surf</h4>
+                </v-col>
+                <v-spacer></v-spacer>
+                <v-col class="pa-0" cols="3">
+                    <v-switch class="ma-0"
+                        v-model="surfZoneFilter"
+                        dense
+                    ></v-switch>
+                </v-col>
+            </v-row>
+        </div>
+<div v-show="placeFilter == 1">
+            <v-row>
+                <v-col style="padding-bottom: 0">
+                    <v-divider style="margin-bottom:20px"></v-divider>
+                    <h4>Tipo de establecimiento</h4>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col>
+                    <v-list>
+                        <v-list-item-group v-model="model">
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>Bar/Cafetería</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>Restarante</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>Guachinche</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list-item-group>
+                    </v-list>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col style="padding-bottom: 0">
+                    <v-divider style="margin-bottom:20px"></v-divider>
+                    <h4>Tipo de comida</h4>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col>
+                    <v-list>
+                        <v-list-item-group v-model="model">
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>Típica</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>Mediterránea</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>Internacional</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list-item-group>
+                    </v-list>
+                </v-col>
+            </v-row>
+                       <v-row>
+                <v-col style="padding-bottom: 0">
+                    <v-divider style="margin-bottom:20px"></v-divider>
+                    <h4>Especialidad</h4>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col>
+                    <v-list>
+                        <v-list-item-group v-model="model">
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>Carne a la parrilla</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>Sopas</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>Pescado frito</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list-item-group>
+                    </v-list>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col style="padding-bottom: 0">
+                    <v-divider style="margin-bottom:20px"></v-divider>
+                    <h4>Comidas</h4>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col>
+                    <v-list>
+                        <v-list-item-group v-model="model">
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>Desayuno</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>Brunch</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>Almuerzo</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>Cena</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list-item-group>
+                    </v-list>
+                </v-col>
+            </v-row>
+            <v-row align-content="center">
+                <v-col class="py-0 pt-1" cols="8">
+                    <h4>Acceso para discapacitados</h4>
+                </v-col>
+                <v-spacer></v-spacer>
+                <v-col class="pa-0" cols="3">
+                    <v-switch class="ma-0"
+                        v-model="disabledAccessFilter"
+                    ></v-switch>
+                </v-col>
+            </v-row>
+            <v-row align-content="center">
+                <v-col class="py-0 pt-1" cols="8">
+                    <h4>Parking</h4>
+                </v-col>
+                <v-spacer></v-spacer>
+                <v-col class="pa-0" cols="3">
+                    <v-switch class="ma-0"
+                        v-model="parkingFilter"
+                    ></v-switch>
+                </v-col>
+            </v-row>
         </div>
     </v-container>
 </template>
@@ -241,6 +559,23 @@
 export default {
     data: () => ({
         islandFilter: null,
+        municipalityFilter: null,
+        placeFilter: null,
+        nudismFilter: null,
+        blueFlagFiler: null,
+        lifeGuardFilter: null,
+        disabledAccessFilter: null,
+        parkingFilter: null,
+        showerFilter: null,
+        rentalSunUmbrellaFilter: null,
+        rentalHamocksFilter: null,
+        rentalBoatsFilter: null,
+        foodFilter: null,
+        drinksFilter: null,
+        childZoneFilter: null,
+        sportZoneFilter: null,
+        scubaDivingFilter: null,
+        surfZoneFilter: null,
         islands: [
             {
                 name: 'El Hierro'
@@ -264,8 +599,6 @@ export default {
                 name: 'Tenerife'
             },
         ],
-        municipalityFilter: null,
-        placeFilter: null,
     }),
     computed: {
         hasSelectedIsland() {
@@ -278,29 +611,29 @@ export default {
             if (this.hasSelectedIsland) {
                 switch(this.selectedIsland){
                     case 'El Hierro':
-                        return ['Frontera', 
-                                'Valverde', 
+                        return ['Frontera',
+                                'Valverde',
                                 'El Pinar de El Hierro'
                                 ]
                     case 'Fuerteventura':
-                        return ['Antigua', 
-                                'Betancuria', 
-                                'La Oliva', 
-                                'Pájara', 
+                        return ['Antigua',
+                                'Betancuria',
+                                'La Oliva',
+                                'Pájara',
                                 'Puerto del Rosario',
                                 'Tuineje'
                                 ]
                     case 'Gran Canaria':
-                        return ['Agaete', 
-                                'Agüimes', 
-                                'Artenara', 
-                                'Arucas', 
-                                'Firgas', 
-                                'Gáldar', 
-                                'Ingenio', 
-                                'Mogán', 
-                                'Moya', 
-                                'Las Palmas de Gran Canaria', 
+                        return ['Agaete',
+                                'Agüimes',
+                                'Artenara',
+                                'Arucas',
+                                'Firgas',
+                                'Gáldar',
+                                'Ingenio',
+                                'Mogán',
+                                'Moya',
+                                'Las Palmas de Gran Canaria',
                                 'San Bartolomé de Tirajana',
                                 'La Aldea de San Nicolás',
                                 'Santa Brígida',
@@ -380,7 +713,7 @@ export default {
                                 'Vilaflor'
                                 ]
                 }
-            } 
+            }
             return []
         },
     }
