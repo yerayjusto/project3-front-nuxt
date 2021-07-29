8c5e69d58e87cc20bc44b20096bd91a82b0f7b2b
Jeremy Camacho Lugo <jyaclu@gmail.com>
2021-07-29 22:23:24 +0100

    final push 2

--- pages/search-rest-1.vue
+++ pages/search-rest-1.vue
@@ -19,9 +19,7 @@
         cols="12"
         xs="12"
         sm="4"
-        md="4"
-        lg="4"
-        xl="3"
+        md="3"
       >
         <CardRestaurantMobile
           v-if="$vuetify.breakpoint.xs"
